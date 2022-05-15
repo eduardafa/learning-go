6f69b09ab5c9cfc97860cb2080e29e86edeecf46
Eduarda França <eduardafandrade@gmail.com>
2022-05-15 12:36:42 -0300

    first commit

---
+++ introducing-go/chapter-2/exercise-3.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`The result of this multiplication is`, 32132*42452)
+}
---
+++ introducing-go/chapter-2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println(len("Hello , World"))
+	fmt.Println("Hello, World"[3])
+	fmt.Println("Hello, " + "World")
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
